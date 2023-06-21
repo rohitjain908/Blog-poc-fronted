439b94333250cad4f6981bc2b4f0a2be49f21845
Rohit Jain <rrohitjee2018@gmail.com>
2023-06-21 14:56:35 +0530

    add sample blog thumbnail

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,25 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div>Hello</div>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <h1>Blogs</h1>
+      <div style = {{borderRadius : '1px'}}>
+          <img className={styles.image_tag} src = "/image-1.webp"/>
+      </div>
+      <div className={styles.info_container} style = {{ width: '30%'}}>
+          <div style={{ marginTop:'6px', display: 'flex', flexDirection: 'row'}}>
+            <div className={`${styles.tag_without_border} ${styles.tag_light}`}>Blog</div>
+            <div className={`${styles.tag_without_border} ${styles.tag_light}`} style={{ paddingLeft:'8px', paddingRight: '8px'}}>•</div>
+            <div className={`${styles.tag_without_border} ${styles.tag_light}`}>Jan 2006</div>
+          </div>
+          <h1 className={styles.heading_medium} style = {{textAlign:'start'}}>The Synaptic Growth Index: Quantifying Growth Momentum of Startups</h1>
+          <div style = {{display: 'flex', justifyContent: 'start', marginTop: '2px'}}>
+            <div className={styles.button_secondary} style = {{padding:'0px',  border: 0}}>Read More <i class="icon icon-Right-arrow-long" className={styles.icon_Right_arrow_long}></i></div>
+          </div>
+        </div>
     </div>
   )
 }
