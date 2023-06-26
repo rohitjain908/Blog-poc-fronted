7dc32499232f24848cc6849f1066ae3720ee6809
Animesh Jaiswal <animeshgkp15@gmail.com>
2023-06-26 17:37:44 +0530

    api integration

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,17 +1,11 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "../../styles/Home.module.scss";
 import BlogThumnail from "./BlogThumbnail";
 import FiltersBar from "./FiltersBar";
 import { SERVERURL } from "../../config";
-
-async function getData() {
-  try {
-    const res = await axios.get(`${SERVERURL}/blogs?populate=thumbnail`);
-    return res.data;
-  } catch (err) {
-    console.log(err);
-  }
-}
+import qs from "qs";
 
 async function getTags() {
   try {
@@ -21,26 +15,88 @@ async function getTags() {
     console.log(err);
   }
 }
+async function getData(query) {
+  try {
+    const res = await axios.get(`${SERVERURL}/blogs?${query}`);
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+function BlogList() {
+  const [blogs, setBlogs] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [tags, setTags] = useState([]);
+  const selectedValue = (tags) => {
+    setSelectedTags(tags);
+    setQueryHandler();
+  };
 
-export default async function Blogs() {
-  const blogs = await getData();
-  const tags = await getTags();
-  console.log(tags);
+  const searchInputHandler = (value) => {
+    setSearchInput(value);
+    setQueryHandler(value);
+  };
+  const [query, setQuery] = useState("populate[0]=thumbnail&populate[1]=tags");
+  useEffect(() => {
+    getData(query)
+      .then((data) => {
+        setBlogs(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [query]);
+
+  useEffect(() => {
+    getTags().then((res) => setTags(res));
+  }, []);
+  const setQueryHandler = (search) => {
+    const qry = qs.stringify(
+      {
+        populate: ["thumbnail", "tags"],
+        filters: {
+          tags: {
+            title: {
+              $in: selectedTags,
+            },
+          },
+          title: {
+            $containsi: search != undefined ? search : searchInput,
+          },
+        },
+      },
+      { encodeValuesOnly: true }
+    );
+    // console.log(qry);
+    setQuery(qry);
+  };
+
   return (
     <div className="flex">
-      <FiltersBar />
+      <FiltersBar
+        tags={tags.data}
+        selectedTags={selectedTags}
+        setSelectedTags={selectedValue}
+        searchInput={searchInput}
+        setSearchInput={searchInputHandler}
+      />
       <div className="max-w-7xl mx-auto">
         <h1 className="text-center">Blogs</h1>
         <div className="grid grid-cols-1 xl:grid-cols-3 mt-20 px-6 xl:px-0 flex-row">
-          {blogs.data.map((blog) => (
+          {blogs?.data?.map((blog) => (
             <BlogThumnail blog={blog} />
           ))}
+          {/* <BlogThumnail />
           <BlogThumnail />
           <BlogThumnail />
-          <BlogThumnail />
-          <BlogThumnail />
+          <BlogThumnail /> */}
         </div>
       </div>
     </div>
   );
+}
+
+export default async function Blogs() {
+  return <BlogList />;
 }
