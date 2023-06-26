73d3becf9b61bdbd7272b0526316b4520f13be13
Animesh Jaiswal <animeshgkp15@gmail.com>
2023-06-26 16:06:38 +0530

    api changes

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -2,33 +2,44 @@ import axios from "axios";
 import styles from "../../styles/Home.module.scss";
 import BlogThumnail from "./BlogThumbnail";
 import FiltersBar from "./FiltersBar";
+import { SERVERURL } from "../../config";
 
 async function getData() {
   try {
-    const res = await axios.get(
-      `http://127.0.0.1:1337/api/blogs?populate=thumbnail`
-    );
+    const res = await axios.get(`${SERVERURL}/blogs?populate=thumbnail`);
     return res.data;
   } catch (err) {
     console.log(err);
   }
 }
+
+async function getTags() {
+  try {
+    const response = await axios.get(`${SERVERURL}/tags`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 export default async function Blogs() {
   const blogs = await getData();
+  const tags = await getTags();
+  console.log(tags);
   return (
     <div className="flex">
-      <FiltersBar/>
+      <FiltersBar />
       <div className="max-w-7xl mx-auto">
-          <h1 className="text-center">Blogs</h1>
-          <div className="grid grid-cols-1 xl:grid-cols-3 mt-20 px-6 xl:px-0 flex-row">
-            {blogs.data.map((blog) => (
-              <BlogThumnail blog={blog} />
-            ))}
-            <BlogThumnail />
-            <BlogThumnail />
-            <BlogThumnail />
-            <BlogThumnail />
-          </div>
+        <h1 className="text-center">Blogs</h1>
+        <div className="grid grid-cols-1 xl:grid-cols-3 mt-20 px-6 xl:px-0 flex-row">
+          {blogs.data.map((blog) => (
+            <BlogThumnail blog={blog} />
+          ))}
+          <BlogThumnail />
+          <BlogThumnail />
+          <BlogThumnail />
+          <BlogThumnail />
+        </div>
       </div>
     </div>
   );
